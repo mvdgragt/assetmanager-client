643d0ce07300c368634a75780d2a62660f27d363
mvdgragt <michiel@vandergragt.eu>
2023-03-29 15:35:55 +0200

    added ./ to nav

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -11,10 +11,10 @@ const Navigation = ({ logoutUser }) => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="listassets">List All Devices</Nav.Link>
+            <Nav.Link href="./listassets">List All Devices</Nav.Link>
             <Nav.Link href="/">List All Assets</Nav.Link>
-            <Nav.Link href="devices">Register Device</Nav.Link>
-            <Nav.Link href="persons">Register Person</Nav.Link>
+            <Nav.Link href="./devices">Register Device</Nav.Link>
+            <Nav.Link href="./persons">Register Person</Nav.Link>
           </Nav>
           <Nav className="ml-auto">
             <Button variant="success" onClick={logoutUser}>Logout</Button>
