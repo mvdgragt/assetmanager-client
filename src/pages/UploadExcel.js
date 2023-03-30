cbc568913cba2270c39bf44feea455c89291b0f5
mvdgragt <michiel@vandergragt.eu>
2023-03-30 11:20:25 +0200

    added navbar

--- src/pages/UploadExcel.js
+++ src/pages/UploadExcel.js
@@ -1,6 +1,10 @@
 const UploadExcel = () => {
     return (
+        <div className="container mt-5">
+        <Navigation logoutUser={logoutUser}/>
+
         <h1>Excel Upload</h1>
+        </div>
     )
 }
 
