bbbe1b2661793114a71cef8198f32e87b44cddcf
mvdgragt <michiel@vandergragt.eu>
2023-03-30 09:13:21 +0200

    added underline navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,22 +6,23 @@ import { Button } from "react-bootstrap";
 const Navigation = ({ logoutUser }) => {
   return (
     <Navbar bg="light" variant="light" expand="lg">
-      <Container>
-        <Navbar.Brand href="/">ISH Assetmanager</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="./listassets">List All Devices</Nav.Link>
-            <Nav.Link href="/">List All Assets</Nav.Link>
-            <Nav.Link href="./devices">Register Device</Nav.Link>
-            <Nav.Link href="./persons">Register Person</Nav.Link>
-          </Nav>
-          <Nav className="ml-auto">
-            <Button variant="success" onClick={logoutUser}>Logout</Button>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <Container>
+      <Navbar.Brand href="/">ISH Assetmanager</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link href="./listassets" className="text-dark text-decoration-none">List All Devices</Nav.Link>
+          <Nav.Link href="/" className="text-dark text-decoration-none">List All Assets</Nav.Link>
+          <Nav.Link href="./devices" className="text-dark text-decoration-none">Register Device</Nav.Link>
+          <Nav.Link href="./persons" className="text-dark text-decoration-none">Register Person</Nav.Link>
+        </Nav>
+        <Nav className="ml-auto">
+          <Button variant="success" onClick={logoutUser}>Logout</Button>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
+  
   );
 };
 
