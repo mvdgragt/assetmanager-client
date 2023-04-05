2f834cdce96e848bd2201b98539c6f6dcb0abcb9
mvdgragt <michiel@vandergragt.eu>
2023-04-05 16:02:18 +0200

    changing fetch link

--- src/pages/ListAssets.js
+++ src/pages/ListAssets.js
@@ -60,7 +60,7 @@ const [toggleCleared, setToggleCleared] = useState(false);
     
    useEffect(() => {
         async function getAssets() {
-            const res = await fetch(`${process.env.REACT_APP_BACKEND_URL}/allAssets`, {
+            const res = await fetch(`assetmanager-server-production.up.railway.app/allAssets`, {
                 headers: {
                     'Authorization': `Bearer ${token}`,                    
                     'Access-Control-Allow-Origin': '*',
@@ -96,7 +96,7 @@ const [toggleCleared, setToggleCleared] = useState(false);
     const submitButton = useCallback(() => {
 console.log(selectedRows)
         selectedRows.forEach(e => 
-            fetch(`${process.env.REACT_APP_BACKEND_URL}/deleteasset/${e.SerialNumber}`, {
+            fetch(`assetmanager-server-production.up.railway.app/deleteasset/${e.SerialNumber}`, {
                 method: "DELETE",
                 headers: 
                 {   "Content-Type": "application/json",
