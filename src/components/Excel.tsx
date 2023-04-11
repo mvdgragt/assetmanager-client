bbc4552ac0bc1c8a81404efa60ba8b59be8bbdf8
mvdgragt <michiel@vandergragt.eu>
2023-04-11 13:44:49 +0200

    changed typescript to Javascript for now

--- src/components/Excel.tsx
+++ src/components/Excel.tsx
@@ -1,9 +1,19 @@
-import { utils, writeFileXLSX } from 'xlsx';
+// import { utils, writeFileXLSX } from 'xlsx';
 
+// export const handleDownloadExcel = (dataSource:any,sheetName:string,fileName:string) => {
+//     const ws = utils.json_to_sheet(dataSource);
+//     const wb = utils.book_new();
+//     utils.book_append_sheet(wb, ws, sheetName);
+//     writeFileXLSX(wb, `${fileName}.xlsx`);        
+// };
 
-export const handleDownloadExcel = (dataSource:any,sheetName:string,fileName:string) => {
-    const ws = utils.json_to_sheet(dataSource);
-    const wb = utils.book_new();
-    utils.book_append_sheet(wb, ws, sheetName);
-    writeFileXLSX(wb, `${fileName}.xlsx`);        
+import * as XLSX from 'xlsx';
+
+const { utils, writeFile } = XLSX;
+
+export const handleDownloadExcel = (dataSource, sheetName, fileName) => {
+  const ws = utils.json_to_sheet(dataSource);
+  const wb = utils.book_new();
+  utils.book_append_sheet(wb, ws, sheetName);
+  writeFile(wb, `${fileName}.xlsx`);
 };
