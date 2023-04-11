70d98f3760ea6179d0d62a22b44df50e8a358c88
mvdgragt <michiel@vandergragt.eu>
2023-04-11 13:32:48 +0200

    testing adding another button

--- src/pages/ListMovements.js
+++ src/pages/ListMovements.js
@@ -103,10 +103,14 @@ selectedRows.forEach(e =>
     const contextActions = useMemo(() => {
  
     return(
-      
+      <div>
         <button className="btn btn-danger" onClick = {submitButton } style={{ backgroundColor: 'red' }}>
 				Book In
 			</button>
+                <button className="btn btn-danger" onClick = {submitButton } style={{ backgroundColor: 'red' }}>
+				Export
+			</button>
+            </div>
     )
     },[submitButton])
 
