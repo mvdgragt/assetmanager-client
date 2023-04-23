d0aedb931bf7124dac81b663abaf27ef70277712
Michiel <47319476+mvdgragt@users.noreply.github.com>
2023-04-23 15:37:16 +0200

    again

--- src/App.js
+++ src/App.js
@@ -31,33 +31,14 @@ function App() {
        // setUser(user)
         console.log("user :",user.displayName)
         console.log("token :", token)
-         if(user.email.endsWith("miva1000@utb.helsingborg.se || sivi1000@utb.helsingborg.se")){
-               // Refresh token logic
-      user.getIdToken(/* forceRefresh */ true).then((tkn)=>{
-        // set access token in session storage
-        sessionStorage.setItem("accessToken", tkn);
-        setAuthorizedUser(true);
-      })
+         if(user.email.endsWith("miva1000@utb.helsingborg.se" || "sivi1000@utb.helsingborg")){
+           user.getIdToken().then((tkn)=>{
+             // set access token in session storage
+             sessionStorage.setItem("accessToken", tkn);
+             setAuthorizedUser(true);
+           })
          }
-   
-
-
-        //Assign who is allowed to login
-        // if(  
-        // user.email.endsWith("miva1000@utb.helsingborg.se") ||
-        // user.email.endsWith("katrinasblogg@gmail.com") ||
-        // user.email.endsWith("sivi1000@utb.helsingborg.se")
-
-        
-        // ){
-        //   user.getIdToken().then((tkn)=>{
-        //     // set access token in session storage
-        //     sessionStorage.setItem("accessToken", tkn);
-        //     setAuthorizedUser(true);
-        //   })
-        // }
-
-
+  
       })
   }
   const logoutUser = () => {
