f8baf51464135cc2caa917b0322a17d909a6d183
Michiel <47319476+mvdgragt@users.noreply.github.com>
2023-04-27 16:36:07 +0200

    Update UploadExcel.js

--- src/pages/UploadExcel.js
+++ src/pages/UploadExcel.js
@@ -3,54 +3,88 @@ import React, { useEffect, useState } from 'react';
 import Navigation from '../components/Navigation';
 import * as XLSX from 'xlsx'
 import DataTable from 'react-data-table-component';
-
-const columns = [
-  {
-    name: 'Asset Number',
-    selector: row => row.assetnumber,
-    sortable: true,
-  },
-  {
-    name: 'Serial Number',
-    selector: row => row.serialnumber,
-    sortable: true,
-  },
-
-  {
-    name: 'Asset Description',
-    selector: row => row.assetdescription,
-    sortable: true,
-  },
-  {
-    name: 'Asset Type Name',
-    selector: row => row.assettypename,
-    sortable: true,
-  },
-  {
-    name: 'Location',
-    selector: row => row.location,
-    sortable: true,
-  },
-  {
-    name: 'Purchase Date',
-    selector: row => row.purchasedate,
-    sortable: true,
-    //  cell: (row) => row.PurchaseDate.substring(0, 10),
-  },
-  {
-    name: 'Purchase Value',
-    selector: row => row.purchasevalue,
-    sortable: true,
-  },
-];
-
+import { Button } from "react-bootstrap";
+// import CircularJSON from 'circular-json'
 
 
 const UploadExcel = ({logoutUser}) => {
+  const columns = [
+    {
+      name: 'Asset Number',
+      selector: row => row.assetnumber,
+      sortable: true,
+    },
+    {
+      name: 'Serial Number',
+      selector: row => row.serialnumber,
+      sortable: true,
+    },
+  
+    {
+      name: 'Asset Description',
+      selector: row => row.assetdescription,
+      sortable: true,
+    },
+    {
+      name: 'Asset Type Name',
+      selector: row => row.assettypename,
+      sortable: true,
+    },
+    {
+      name: 'Location',
+      selector: row => row.location,
+      sortable: true,
+    },
+    {
+      name: 'Purchase Date',
+      selector: row => row.purchasedate,
+      sortable: true,
+      //  cell: (row) => row.PurchaseDate.substring(0, 10),
+    },
+    {
+      name: 'Purchase Value',
+      selector: row => row.purchasevalue,
+      sortable: true,
+    },
+    {
+      name: 'Cost Center',
+      selector: row => row.costcenter,
+      cell: row => (
+        <select 
+        className="form-select-sm"
+        defaultValue={row.costcenter || ''}
+          onChange={e => {
+            const newCostCenter = e.target.value;
+            const assetnumber = row.assetnumber;
+            console.log(assetnumber)
+            const updatedData = newAssets.map(d => {
+            //  console.log('d.assetnumber:', d.assetnumber, 'row.assetnumber:', row.assetnumber);
+              if (d.assetnumber === assetnumber) {
+                return { ...d, costcenter: newCostCenter };
+              }
+              return d;
+            });
+            setNewAssets(updatedData);
+            console.log(updatedData.slice(0,10))
+          }}
+        >
+          <option value="" disabled={!row.costcenter}>Choose</option>
+          <option value="EY">EY</option>
+          <option value="PYP">PYP</option>
+          <option value="MYP">MYP</option>
+          <option value="DP">DP</option>
+        </select>
+      ),
+    },
+  ];
+
   // on change states
   const [excelFile, setExcelFile] = useState(null);
   const [excelFileError, setExcelFileError] = useState(null);
-  const [totalCost,setTotalCost] = useState(null)
+  const [totalCost,setTotalCost] = useState(null);
+  const [newAssets,setNewAssets] = useState([]);
+  const [pending, setPending] = useState(false);
+
   // const [payThisMonth,setPayThisMonth] = useState(0);
 
   // submit
@@ -62,6 +96,7 @@ const UploadExcel = ({logoutUser}) => {
     let selectedFile = e.target.files[0];
     if (selectedFile) {
       if (selectedFile && fileType.includes(selectedFile.type)) {
+        setPending(true);
         let reader = new FileReader();
         reader.readAsArrayBuffer(selectedFile);
         reader.onload = (e) => {
@@ -106,9 +141,11 @@ const UploadExcel = ({logoutUser}) => {
 
         return {
           ...object,
-          purchasedate: formattedDate
+          purchasedate: formattedDate,
         };
-      });
+      }
+
+      );
   
       const totalCost = updatedObjects.reduce((acc, asset) => {
         if (asset && asset.totalcost) {
@@ -128,9 +165,19 @@ const UploadExcel = ({logoutUser}) => {
 
 const batchSize = 20; // Number of objects to send in each batch
 const numBatches = Math.ceil(updatedObjects.length / batchSize);
-console.log(numBatches)
-// const assets = updatedObjects
-// console.log("assets", assets)
+console.log("numbatches :", numBatches)
+
+ //const assets = updatedObjects
+ //console.log("assets", assets)
+
+ fetch(`${process.env.REACT_APP_BACKEND_URL}/truncateMonthlyUpload`, {
+  method: "POST",
+  headers: {
+    "Content-type": "application/json", 'Authorization': `Bearer ${token}`, 
+  }
+});
+
+
 
 //this is where the data is passed to the server
 const uploadBatch = async (batch) => {
@@ -142,17 +189,59 @@ const uploadBatch = async (batch) => {
       },
       body: JSON.stringify(batch),
     });
+
    // console.log(response)
     if (!response.ok) {
       throw new Error("Upload failed");
     }
     console.log(`Batch uploaded successfully`);
-    const data = await response.json()
-    console.log(data)
+
   } catch (error) {
     console.error(error);
   }
 };
+
+const updateAssetsTable = async () => {
+  console.log("updating assetstable")
+
+ const result =  await fetch(`${process.env.REACT_APP_BACKEND_URL}/truncateAssetsNotInSpreadsheet`, {
+    method: "DELETE",
+    headers: {
+        'Authorization': `Bearer ${token}`,                    
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',                
+    }
+});
+ const removingAssets = await result.json()
+ console.log("removing : ", removingAssets)
+
+// const result =  await fetch(`${process.env.REACT_APP_BACKEND_URL}/truncateAssetsNotInSpreadsheet`, {
+//   method: "GET",
+//   headers: {
+//       'Authorization': `Bearer ${token}`,                    
+//       'Access-Control-Allow-Origin': '*',
+//       'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',                
+//   }
+// });
+// const removingAssets = await result.json()
+// console.log("removing : ", removingAssets)
+ 
+  const res = await fetch(`${process.env.REACT_APP_BACKEND_URL}/showNewAssets`, {
+    headers: {
+        'Authorization': `Bearer ${token}`,                    
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',                
+    }
+});
+const newAssets = await res.json();
+  const timeout = setTimeout(() => {
+    setNewAssets(newAssets);
+    setPending(false);
+  }, 2000);
+  return () => clearTimeout(timeout)
+}
+
+
 
 const uploadData =  () => {
   for (let i = 0; i < numBatches; i++) {
@@ -162,12 +251,11 @@ const uploadData =  () => {
    // const batch = JSON.stringify(batch2)
    // console.log(batch)
 
-     uploadBatch(batch);
+     batch.forEach( async (object) => await uploadBatch(object));
 
 
   }
-  console.log(`Batch uploaded successfully`);
-
+  updateAssetsTable()
 };
 
 uploadData();
@@ -176,6 +264,50 @@ uploadData();
     }
   }, [excelFile]);
   
+   // monthlyUploadToAssets
+   const submit = async (token) => {
+   // console.log("break down assets as with the monthlyupload")
+   console.log(newAssets);
+   try {
+    const assettypeMapping = {
+      Mobiltelefon: 10,
+      Chromebook: 6,
+      "Bärbar dator": 4,
+      Dockingsenhet: 18,
+      Bildskärm: 13,
+      Skanner: 11,
+    }
+
+    const updatedAssets = newAssets.map((object) => {
+      const assetTypeID = assettypeMapping[object.assettypename];
+      if(assetTypeID) {
+        return {...object, assetTypeID}
+      } else {
+        return object
+      }
+    })
+
+    updatedAssets.forEach( async (object) =>  await fetch(`${process.env.REACT_APP_BACKEND_URL}/monthlyUploadToAssets`, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json", 'Authorization': `Bearer ${token}`, 
+      },
+      body: JSON.stringify(object),
+    }));
+
+   // console.log(response)
+    // if (!response.ok) {
+    //   throw new Error("Upload failed");
+    // }
+    console.log(newAssets);
+
+  } catch (error) {
+    console.error(error);
+  }
+  }
+
+
+    
   return (
     <div className="container mt-5">
       <Navigation logoutUser={logoutUser} />
@@ -187,30 +319,52 @@ uploadData();
         <br></br>
         <input type='file' className='form-control'
           onChange={handleFile} required></input>
+          <br></br>
         {excelFileError && <div className='text-danger'
           style={{ marginTop: 5 + 'px' }}>{excelFileError}</div>}
         {/* <button type='submit' className='btn btn-success'
           style={{ marginTop: 5 + 'px' }}>Submit</button> */}
       </form>
 
-      <DataTable
-        title="All Devices from File Upload"
-        defaultSortFieldId={6}
+<>
+      {totalCost && 
+      <div className="d-flex justify-content-between container mt-5">
+       {/* <div className="d-flex justify-content-between align-items-center container mt-5"> */}
+      <h4>To pay this month: {totalCost} SEK</h4>    
+
+      </div>
+
+      }
+
+
+{excelData !== 0 && <DataTable
+        title="New Devices"
+        defaultSortFieldId="PurchaseDate"
         defaultSortAsc={false}
-        pagination
+       // pagination
+        progressPending={pending}
         columns={columns}
-        data={excelData}
-        selectableRows
+        data={newAssets}
         selectableRowsHighlight
+        highlightOnHover
         // onSelectedRowsChange={handleSelected}
         // contextActions={contextActions}
         // clearSelectedRows={toggleCleared}
         responsive
-      />
+      />}
+      
+      </>     
+     
+      <div className="d-grid gap-2 col-6 mx-auto">
+       
+      <Button variant="success" className="btn-lg" onClick={submit}>Add</Button>
+
+      </div>
+
+
+  
 
 <hr />
-{totalCost && <h4>To pay this month: {totalCost} SEK</h4>}    
-
 </div>
   )
 }
